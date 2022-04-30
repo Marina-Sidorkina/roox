2f06e2d29560e1fe460d2816e0423e74bd98dc43
Marina Sidorkina <m.sidorkina55@gmail.com>
2022-04-30 21:45:57 +0300

    Add deploy.

---
+++ src/firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDbvRMEZ_XGqXd56fzPx3CJVUjLNZ2XVxw',
+  authDomain: 'roox-test.firebaseapp.com',
+  projectId: 'roox-test',
+  storageBucket: 'roox-test.appspot.com',
+  messagingSenderId: '953820376578',
+  appId: '1:953820376578:web:d4f77c86c8322ed9fcc11b',
+};
+
+// Initialize Firebase
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const app = initializeApp(firebaseConfig);
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './containers/App';
 import './index.scss';
+import './firebase';
 
 ReactDOM.render(
   <Provider store={store}>
