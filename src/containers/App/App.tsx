f8c0da1ff4d1572722f2ceab03dcd95a53b6ef1b
Marina Sidorkina <m.sidorkina55@gmail.com>
2022-04-30 19:07:55 +0300

    Add fixes.

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,44 +1,33 @@
 import * as React from 'react';
-import { useDispatch } from 'react-redux';
-import { useEffect } from 'react';
 import {
   HashRouter, Redirect, Route, Switch,
 } from 'react-router-dom';
 import FilterContainer from '../FilterContainer';
 import UsersListContainer from '../UsersListContainer';
-import { loadUsersListAction } from '../../redux/actions/usersList';
 import Sidebar from '../../components/Sidebar';
 import Page from '../../components/Page';
 import Content from '../../components/Content';
 import ProfileContainer from '../ProfileContainder';
 
-const App = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(loadUsersListAction());
-  }, []);
-
-  return (
-    <Page>
-      <HashRouter>
-        <Sidebar>
-          <FilterContainer />
-        </Sidebar>
-        <Content>
-          <Switch>
-            <Route path="/users">
-              <UsersListContainer />
-            </Route>
-            <Route path="/profile/:id">
-              <ProfileContainer />
-            </Route>
-            <Redirect from="/" to="/users" />
-          </Switch>
-        </Content>
-      </HashRouter>
-    </Page>
-  );
-};
+const App = () => (
+  <Page>
+    <HashRouter>
+      <Sidebar>
+        <FilterContainer />
+      </Sidebar>
+      <Content>
+        <Switch>
+          <Route path="/users">
+            <UsersListContainer />
+          </Route>
+          <Route path="/profile/:id">
+            <ProfileContainer />
+          </Route>
+          <Redirect from="/" to="/users" />
+        </Switch>
+      </Content>
+    </HashRouter>
+  </Page>
+);
 
 export default App;
